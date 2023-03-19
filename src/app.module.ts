863deb315b5283ff0ca4f9176505b66c61febcc0
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-03-19 15:09:08 +0200

    init

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import {ConfigModule} from "@nestjs/config";
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users/users.model";
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+    envFilePath: `.${process.env.NODE_ENV}.env`,
+    // load: [configuration],
+  }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User],
+      autoLoadModels: true,
+    }),
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = +process.env.SERVER_PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port, () => console.log(`Server started on port: ${port}`));
+}
+bootstrap();
