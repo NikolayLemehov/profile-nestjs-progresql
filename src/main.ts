de6937873f3d97f5d9db0191c6206aa2927824c3
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-03-20 12:03:28 +0200

    feat: start swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = +process.env.SERVER_PORT || 5000;
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User profile')
+    .setDescription('one-to-one, nest, rest')
+    .setVersion('1.0.0')
+    // .addTag('asdf')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
   await app.listen(port, () => console.log(`Server started on port: ${port}`));
 }
 bootstrap();
